d3d596d44fd83fefd2c002a86d5be7788c8546a6
oherbert <o_herbert@hotmail.com>
2024-03-24 18:15:07 -0300

    update

--- src/routes/document.routes.ts
+++ src/routes/document.routes.ts
@@ -9,6 +9,12 @@ const router: Router = Router();
 
 router.post('/pdf/encrypt', upload.single('arquivo'), pdfManagerController, tempCleaner);
 
+router.get('/pdf/teste', (_, res) => {
+
+  return res.sendFile('C:/Users/o_her/OneDrive/Documentos/A Arte da Guerra #@123@#.pdf');
+
+})
+
 router.get('/', (_: Request, res: Response) => {
   return res.status(200).send({
     mensagem: 'Serviço On-line',
